16e6d389a887a806da5e5c88e5d3529b7e29761d
Eduardo Fernández (Mac) <feguedi@yahoo.com.mx>
2019-12-14 16:58:13 -0600

    Gráficas de dólar y diesel con sus tablas de datos

--- js/components/LineChart.jsx
+++ js/components/LineChart.jsx
@@ -1,1 +1,62 @@
-import {  } from "react-chartjs-2"
+import { Line } from "react-chartjs-2"
+import axios from 'axios'
+
+export default class LineChart extends React.Component {
+    state = {
+        chartRef: {}
+    }
+
+    async getData() {
+        const dataDiesel = await (await axios.get(`http://localhost:3003/diesel`)).data
+        const dataDollar = await (await axios.get(`http://localhost:3003/dollar`)).data
+
+        dataDiesel.forEach(d => {
+            let nd = new Date(d["FECHA"])
+            d["FECHA"] = `${ nd.getDate() }/${ nd.getMonth() + 1 }/${ nd.getFullYear() }`
+        })
+        dataDollar.forEach(d => {
+            let nd = new Date(d["FECHA"])
+            d["FECHA"] = `${ nd.getDate() }/${ nd.getMonth() + 1 }/${ nd.getFullYear() }`
+        })
+
+        const dollarDataset = {
+            label: "Dólar",
+            data: dataDollar.map(d => d["MEDIA"]),
+            fill: 'none',
+            borderColor: '#4caf50',
+            borderWidth: 1,
+            borderCapStyle: "butt",
+            lineTension: 1
+        }
+        const dieselDataset = {
+            label: "Diesel",
+            data: dataDiesel.map(d => d["PRECIO"]),
+            fill: 'none',
+            borderColor: '#424242',
+            borderWidth: 1,
+            borderCapStyle: "butt",
+            lineTension: 1
+        }
+
+        this.setState({
+            chartRef: {
+                labels: [dataDiesel[0]["FECHA"], dataDiesel[31]["FECHA"], dataDiesel[59]["FECHA"], dataDiesel[90]["FECHA"], dataDiesel[120]["FECHA"]],
+                datasets: [dollarDataset, dieselDataset]
+            }
+        })
+    }
+
+    async componentDidMount() {
+        await this.getData()
+        const { chartRef } = this.state
+        console.log(`Datos de dólar: ${ JSON.stringify(chartRef.datasets[0].data) }`)
+        console.log(`Datos de diesel: ${ JSON.stringify(chartRef.datasets[1].data) }`)
+    }
+
+    render() {
+        const { chartRef } = this.state
+        return (
+            <Line ref="chart" data={ chartRef } />
+        )
+    }
+}
--- js/pages/data.jsx
+++ js/pages/data.jsx
@@ -1,44 +1,75 @@
 import Layout from "../components/Layout"
-import { Table } from 'react-bootstrap'
+import LineChart from '../components/LineChart'
+import { Table, Row } from 'react-bootstrap'
 import axios from "axios"
 
-const Data = () => (
+const Data = ({ dieselData, dollarData }) => (
     <Layout>
-        <h1>Diesel</h1>
-        <Table className="table-lg-responsive table-hover">
-            <thead className="thead-dark">
-                <tr>
-                    <th>Fecha</th>
-                    <th>Precio</th>
-                </tr>
-            </thead>
-            <tbody>
-                { dieselComponent }
-            </tbody>
-        </Table>
+        <Row>
+            <div className="col-12 col-md-6">
+                <h1 className="text-center">Diesel</h1>
+                <Table className="table-lg-responsive table-hover">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>Fecha</th>
+                            <th>Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { dieselData.map(obj => (
+                            <tr key={ obj["ID"] }>
+                                <td>{ obj["FECHA"] }</td>
+                                <td>{ obj["PRECIO"] }</td>
+                            </tr>
+                        )) }
+                    </tbody>
+                </Table>
+            </div>
+            <div className="col-12 col-md-6">
+                <h1 className="text-center">Dólar</h1>
+                <Table className="table-lg-responsive table-hover">
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>Fecha</th>
+                            <th>Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { dollarData.map(obj => (
+                            <tr key={ obj["ID"] }>
+                                <td>{ obj["FECHA"] }</td>
+                                <td>{ obj["MEDIA"] }</td>
+                            </tr>
+                        )) }
+                    </tbody>
+                </Table>
+            </div>
+        </Row>
+        <LineChart />
     </Layout>
 )
 
-let dieselComponent
-
-axios.get('localhost:3003/diesel')
-    .then(({ data }) => {
-        console.log(`Datos: ${ data }`)
-        dieselComponent = data.map(obj => (
-            <tr>
-                <td>{ obj["FECHA"] }</td>
-                <td>{ obj["PRECIO"] }</td>
-            </tr>
-        ))
+Data.getInitialProps = async () => {
+    const dieselData = await (await axios.get('http://localhost:3003/diesel')).data
+    const dollarData = await (await axios.get('http://localhost:3003/dollar')).data
+    dieselData.forEach(d => {
+        const nd = JSON.stringify(d["FECHA"])
+        const year = nd.split("-")[0].slice(1, 5)
+        const month = nd.split("-")[1]
+        const day = nd.split("-")[2].slice(0, 2)
+        d["FECHA"] = `${ day }/${ month }/${ year }`
     })
-    .catch(err => {
-        console.error(`${ JSON.stringify(err) }`)
-        dieselComponent = (
-            <tr>
-                <td>-</td>
-                <td>-</td>
-            </tr>
-        )
+    dollarData.forEach(d => {
+        const nd = JSON.stringify(d["FECHA"])
+        const year = nd.split("-")[0].slice(1, 5)
+        const month = nd.split("-")[1]
+        const day = nd.split("-")[2].slice(0, 2)
+        d["FECHA"] = `${ day }/${ month }/${ year }`
     })
+    return {
+        dieselData,
+        dollarData
+    }
+}
 
 export default Data
