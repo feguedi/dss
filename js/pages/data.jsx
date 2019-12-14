e2dd77ba98fc84bc65fc285deb678c9377aaffeb
Eduardo Fernández (Mac) <feguedi@yahoo.com.mx>
2019-12-14 04:49:33 -0600

    Solicitando datos al webservice con Axios

--- js/pages/data.jsx
+++ js/pages/data.jsx
@@ -1,13 +1,44 @@
 import Layout from "../components/Layout"
+import { Table } from 'react-bootstrap'
 import axios from "axios"
 
 const Data = () => (
-    <Layout mt="1em">
+    <Layout>
         <h1>Diesel</h1>
-
+        <Table className="table-lg-responsive table-hover">
+            <thead className="thead-dark">
+                <tr>
+                    <th>Fecha</th>
+                    <th>Precio</th>
+                </tr>
+            </thead>
+            <tbody>
+                { dieselComponent }
+            </tbody>
+        </Table>
     </Layout>
 )
 
-const getData = async () => axios.get('http://localhost:3003/diesel')
+let dieselComponent
+
+axios.get('localhost:3003/diesel')
+    .then(({ data }) => {
+        console.log(`Datos: ${ data }`)
+        dieselComponent = data.map(obj => (
+            <tr>
+                <td>{ obj["FECHA"] }</td>
+                <td>{ obj["PRECIO"] }</td>
+            </tr>
+        ))
+    })
+    .catch(err => {
+        console.error(`${ JSON.stringify(err) }`)
+        dieselComponent = (
+            <tr>
+                <td>-</td>
+                <td>-</td>
+            </tr>
+        )
+    })
 
 export default Data
